08df23bac3a98ea85828c07896e0ace012eb976d
Mordechaie <mordynys@gmail.com>
2024-11-22 00:05:23 -0500

    custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
     
