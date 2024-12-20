b36a98b1e0181c696d72124dfa405c5cc6bab842
Mordechaie <mordynys@gmail.com>
2024-12-20 00:07:56 -0500

    chapter 14 till mixins

--- articles/models.py
+++ articles/models.py
@@ -8,4 +8,6 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('article_detail', kwargs={'pk': self.pk})
 
--- articles/views.py
+++ articles/views.py
@@ -30,5 +30,8 @@ class ArticleCreateView(CreateView):
     fields = (
         "title",
         "body",
-        "author",
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign the logged-in user 
+        return super().form_valid(form)
